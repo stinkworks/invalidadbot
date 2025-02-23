0c4f187a3f38eec95ea7cbb89cdb9db1f8acd9b6
effygp <facundos111@gmail.com>
2025-02-22 21:21:26 -0300

    feat: adds connection to microservices to ensure friends dont fuck up

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,6 +10,7 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+	"time"
 
 	//"invcatter/cataas"
 
@@ -24,10 +26,7 @@ func main() {
 		bot.WithMessageTextHandler("/tag", bot.MatchTypePrefix, handlerSendPhotoByTag),
 		bot.WithMessageTextHandler("/help", bot.MatchTypeExact, handlerHelp),
 		bot.WithMessageTextHandler("/start", bot.MatchTypeExact, handlerHelp),
-		// Add new handler for all messages
-		bot.WithMessageTextHandler("", bot.MatchTypeContains, handlerTwitterLinks),
-		//bot.WithMessageTextHandler("/tag", bot.MatchTypeExact, handlerHelp),
-		//bot.WithMessageTextHandler("/id", bot.MatchTypeExact, handlerHelp),
+		bot.WithMessageTextHandler("", bot.MatchTypeContains, handlerMessage),
 	}
 
 	TelegramBot := initializeBot(botOptions)
@@ -69,7 +68,7 @@ func handlerHelp(ctx context.Context, telegramBot *bot.Bot, update *models.Updat
 	if _, err := telegramBot.SendMessage(ctx, &bot.SendMessageParams{
 		ChatID: update.Message.Chat.ID,
 		Text: "Heya! This bot is mostly made to send cats to people.\n" +
-			"'twas but made by @fecupacufacu; feel free to reach 'em out" +
+			"'twas but made by @effygp; feel free to reach 'em out" +
 			" in Telegram.\n" +
 			"Try some of the following commands:\n" +
 			"/cat (Will send a random cat.)\n" +
@@ -178,12 +177,66 @@ func handlerSendPhotoByTag(ctx context.Context, tgBot *bot.Bot, update *models.U
 
 }
 
-func handlerTwitterLinks(ctx context.Context, tgBot *bot.Bot, update *models.Update) {
+func handlerMessage(ctx context.Context, tgBot *bot.Bot, update *models.Update) {
 	// Regular expression to match x.com URLs
 	twitterRegex := regexp.MustCompile(`https?://(?:www\.)?x\.com/[^/]+/status/[0-9]+`)
-
 	// Find all matches in the message
 	matches := twitterRegex.FindAllString(update.Message.Text, -1)
+
+	// Check if the message is from a group chat and contains the specified chat ID
+	if update.Message.Chat.Type == "group" || update.Message.Chat.Type == "supergroup" {
+		// Get the chat ID from the environment variable
+		specifiedChatID := os.Getenv("CENSORED_CHAT_ID")
+		if specifiedChatID == "" {
+			log.Print("SPECIFIED_CHAT_ID environment variable is not set.")
+			return
+		}
+
+		// Check if the chat ID matches the specified chat ID
+		if strconv.FormatInt(update.Message.Chat.ID, 10) == specifiedChatID {
+			// Prepare the JSON payload
+			messagePayload := fmt.Sprintf(`{"message": "%s"}`, update.Message.Text)
+
+			// Send the message to the specified localhost endpoint
+			endpoint := os.Getenv("CUMCEN_ENDPOINT")
+			resp, err := http.Post(endpoint, "application/json", strings.NewReader(messagePayload))
+			if err != nil {
+				log.Print("Failed to send message to localhost; error: ", err.Error())
+				return
+			}
+			defer resp.Body.Close()
+
+			// Decode the response
+			var response struct {
+				ThreatProbability float64 `json:"threat_probability"`
+			}
+			if err := json.NewDecoder(resp.Body).Decode(&response); err != nil {
+				log.Print("Failed to decode response; error: ", err.Error())
+				return
+			}
+
+			// Check the threat probability
+			if response.ThreatProbability >= 85 {
+				// Notify the chat about the message deletion
+				tgBot.SendMessage(ctx, &bot.SendMessageParams{
+					ChatID: update.Message.Chat.ID,
+					Text:   "Warning: This message will be deleted in 15 seconds due to high threat probability.",
+				})
+
+				// Run the deletion in a goroutine
+				go func() {
+					time.Sleep(15 * time.Second)
+
+					// Delete the message
+					tgBot.DeleteMessage(ctx, &bot.DeleteMessageParams{
+						ChatID:    update.Message.Chat.ID,
+						MessageID: update.Message.ID,
+					})
+				}()
+			}
+			return
+		}
+	}
 
 	if len(matches) > 0 {
 		replacedLinks := make([]string, len(matches))
