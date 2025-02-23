f0f2e56385f1135e18fe32d992867e9f6bc4a24a
Gaspar <52015401+G4sp4rCS@users.noreply.github.com>
2025-02-23 18:31:20 -0300

    Feat: isReply Function
    and some untested isReply

--- main.go
+++ main.go
@@ -79,14 +79,12 @@ func handlerHelp(ctx context.Context, telegramBot *bot.Bot, update *models.Updat
 }
 
 func handlerSendPhoto(ctx context.Context, tgBot *bot.Bot, update *models.Update) {
-	// TODO: Set actual logic whenever the message is a reply lmao
-	/*
-		    if update.Message.ReplyToMessage != nil {
-			handlerSendPhoto(ctx, bot, update)
-		    } else {
-
-		    }
-	*/
+	
+	if isReply(update) {
+		// if is a reply, send a totally random cat as a quote to the replied message
+		log.Print("-- Detected Reply: fetching random cat from:" + "https://cataas.com/cat")
+		apiResponse, err := http.Get("https://cataas.com/cat")
+	}
 	apiResponse, err := http.Get("https://cataas.com/cat")
 	if err != nil {
 		log.Print("-- Failed to fetch cat; error: ", err.Error())
@@ -114,15 +112,6 @@ func handlerSendPhoto(ctx context.Context, tgBot *bot.Bot, update *models.Update
 }
 
 func handlerSendPhotoByTag(ctx context.Context, tgBot *bot.Bot, update *models.Update) {
-	// TODO: Set actual logic whenever the message is a reply lmao
-	/*
-		    if update.Message.ReplyToMessage != nil {
-			handlerSendPhoto(ctx, bot, update)
-		    } else {
-
-		    }
-	*/
-
 	tagToFetch := strings.TrimSpace(regexp.MustCompile(`^\/tag `).ReplaceAllString(update.Message.Text, `${1}`))
 	if tagToFetch == "/tag" {
 		if _, nestedErr := tgBot.SendMessage(ctx, &bot.SendMessageParams{
@@ -273,4 +262,8 @@ func handlerGroupMessage(ctx context.Context, tgBot *bot.Bot, update *models.Upd
 			log.Printf("Failed to send vxtwitter link: %v", err)
 		}
 	}
+}
+
+func isReply(update *models.Update) bool {
+	return update.Message.ReplyToMessage != nil && update.Message.ReplyToMessage != nil
 }
